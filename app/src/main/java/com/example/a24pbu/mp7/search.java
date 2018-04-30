a1dda5591f857427fe8514389bb34662842f05e3
Pburke5 <pburke5@illinois.edu>
2018-04-30 12:40:04 -0500

    Volley still not operational

--- app/src/main/java/com/example/a24pbu/mp7/search.java
+++ app/src/main/java/com/example/a24pbu/mp7/search.java
@@ -25,5 +25,53 @@ public class search extends AppCompatActivity {
             }
         });
     }
+    public class HttpUtils {
+        private static final String BASE_URL = "http://api.twitter.com/1/";
+
+        private static AsyncHttpClient client = new AsyncHttpClient();
+
+        public static void get(String url, RequestParams params, AsyncHttpResponseHandler responseHandler) {
+            client.get(getAbsoluteUrl(url), params, responseHandler);
+        }
+
+        public static void post(String url, RequestParams params, AsyncHttpResponseHandler responseHandler) {
+            client.post(getAbsoluteUrl(url), params, responseHandler);
+        }
+
+        public static void getByUrl(String url, RequestParams params, AsyncHttpResponseHandler responseHandler) {
+            client.get(url, params, responseHandler);
+        }
+
+        public static void postByUrl(String url, RequestParams params, AsyncHttpResponseHandler responseHandler) {
+            client.post(url, params, responseHandler);
+        }
+
+        private static String getAbsoluteUrl(String relativeUrl) {
+            return BASE_URL + relativeUrl;
+        }
+    }
+    RequestParams rp = new RequestParams();
+    rp.add("username", "aaa"); rp.add("password", "aaa@123");
+
+    HttpUtils.post(AppConstant.URL_FEED, rp, new JsonHttpResponseHandler() {
+        @Override
+        public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
+            // If the response is JSONObject instead of expected JSONArray
+            Log.d("asd", "---------------- this is response : " + response);
+            try {
+                JSONObject serverResp = new JSONObject(response.toString());
+            } catch (JSONException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+        }
+
+        @Override
+        public void onSuccess(int statusCode, Header[] headers, JSONArray timeline) {
+            // Pull out the first event on the public timeline
+
+        }
+    });
+    <uses-permission android:name="android.permission.INTERNET" />
 
 }
